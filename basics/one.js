06fe237b1e14b8045cb70b0427be5e917500aa87
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-08 12:16:12 +0530

    basics

---
+++ basics/one.js
@@ -0,0 +1,6 @@
+console.log("Hello, Welcome to JS World");
+
+var id=101
+var name ="Rahul Gandhi...."
+
+console.log(id,"Employee Name:", name)
---
+++ basics/two.js
@@ -0,0 +1,10 @@
+var id9;
+function add(){} 
+
+
+var Id = 101;
+var id = 102;
+console.log(Id); 
+console.log(id); 
+
+function while1(){}
