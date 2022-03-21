c5fde793316936ed9a0c25b99e6aaf8eafb3f969
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-21 12:20:41 +0530

    okie

---
+++ Part2/Obj/emp.js
@@ -0,0 +1,12 @@
+let emp = {
+    id: 101,
+    name: "Rahul",
+    getDetails: function () {
+        return "GM"
+    }
+}
+console.log(emp.id)
+console.log(emp.name)
+console.log(emp.sal)  // undefined
+console.log(emp.getDetails())
+console.log(emp.getBal())
