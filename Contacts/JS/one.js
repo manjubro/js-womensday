7adb3203e7ef190c8a28ba8ecbe4777503800643
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-24 12:32:20 +0530

    okie

---
+++ Contacts/JS/one.js
@@ -0,0 +1,7 @@
+let name = "Rahul Gandhi"
+let salary = 45000
+
+//console.log("Employee Name :" + name + "Employee Salary : " + salary);
+//console.log(`Employee Name L  ${name} Employee Salary :  ${salary}`)
+console.log("Rahul Gandhi".substring(2, 16))
+console.log("   Rahul Gandhi    ".trim().length)
